850545a7915386a57a65cbab16585047fccc7f05
tenhuner <63654635+tenhuner@users.noreply.github.com>
2024-06-25 15:07:48 +0300

    sd

--- src/main/java/main/EventLogger.java
+++ src/main/java/main/EventLogger.java
@@ -18,23 +18,19 @@ public class EventLogger {
     public void writeLog(String logText) {
         LocalDateTime date = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss");
-        try {
-            BufferedWriter logWriter = new BufferedWriter(new FileWriter(FILENAME, true));
+        try (BufferedWriter logWriter = new BufferedWriter(new FileWriter(FILENAME, true))) {
             logWriter.write("[" + date.format(formatter) + "] " + logText + "\n");
-            logWriter.close();
         } catch (IOException e) {
             System.out.println("Virhe kirjoitettaessa logitiedostoa");
         }
     }
 
     public void readLog() {
-        try {
-            BufferedReader logReader = new BufferedReader(new FileReader(FILENAME));
+        try (BufferedReader logReader = new BufferedReader(new FileReader(FILENAME))) {
             String line;
             while ((line = logReader.readLine()) != null) {
                 System.out.println(line);
             }
-            logReader.close();
         } catch (IOException e) {
             System.out.println("Virhe luettaessa logitiedostoa");
         }
